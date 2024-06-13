7020b4602a472aa634df8ae4defb554e2b6395bf
Phuc Hau <haunhpr024@gmail.com>
2024-06-13 20:28:18 +0700

    navbar css

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,28 +1,15 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '../globals.css';
-import Header from '@/Components/user/layouts/Header';
 import Footer from '@/Components/user/layouts/Footer';
+import Header from '@/Components/user/layouts/Header';
+import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-    title: 'My Store',
-    description: 'My Store',
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <Header />
+            <h1>Home</h1>
+            <Footer />
+        </>
+    );
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en">
-            <Header />
-            <body className={inter.className}>
-                <main>{children}</main>
-                <Footer />
-            </body>
-        </html>
-    );
-}
+export default HomeLayout;
