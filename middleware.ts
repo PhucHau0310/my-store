58b53b0990de64611c05b3cb9c044919cbb42347
Phuc Hau <haunhpr024@gmail.com>
2024-06-14 17:28:06 +0700

    not protected route

--- middleware.ts
+++ middleware.ts
@@ -9,11 +9,13 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isPublicRoute = createRouteMatcher(['/api/webhooks(.*)']);
 
 // Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware((auth, req) => {
-    if (!isPublicRoute(req)) {
-        auth().protect();
-    }
-});
+// export default clerkMiddleware((auth, req) => {
+//     if (!isPublicRoute(req)) {
+//         auth().protect();
+//     }
+// });
+
+export default clerkMiddleware();
 
 export const config = {
     matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
